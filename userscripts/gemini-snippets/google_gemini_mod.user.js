f677fc91cb3b08e6cbbe486ffeb23c916dd0ec08
Adromir <adromir@gmx.de>
2026-01-20 14:59:03 +0100

    Remove SRI hash from utils require to prevent loading errors

--- userscripts/gemini-snippets/google_gemini_mod.user.js
+++ userscripts/gemini-snippets/google_gemini_mod.user.js
@@ -18,7 +18,7 @@
 // @require       https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js
 // @require       https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js
 // @require       https://cdn.jsdelivr.net/gh/adromir/scripts@ef4eeb9853f8d32d5cff2f37133fe8bddfb19972/userscripts/gemini-snippets/gemini_mod_styles.js#sha256-bXQDm5Zj7+t4jYaFvTGvx/jUgo08EQmLdHve9CIRVoQ=
-// @require       https://cdn.jsdelivr.net/gh/adromir/scripts@26256554aa603016db08df027558e99c92264515/userscripts/gemini-snippets/gemini_mod_utils.js#sha256-1vKGUr1/Sfudm8jDqC13lCefz4z8xeU8MSyQ8ooUWxNI=
+// @require       https://cdn.jsdelivr.net/gh/adromir/scripts@26256554aa603016db08df027558e99c92264515/userscripts/gemini-snippets/gemini_mod_utils.js
 // @require       https://cdn.jsdelivr.net/gh/adromir/scripts@ef4eeb9853f8d32d5cff2f37133fe8bddfb19972/userscripts/gemini-snippets/gemini_mod_drive.js#sha256-Sf+ByWwt60J4l8gAbxP7jzBd004RVfvwpWhkeyWUcmg=
 // @downloadURL   https://github.com/adromir/scripts/raw/refs/heads/main/userscripts/gemini-snippets/google_gemini_mod.user.js
 // @updateURL     https://github.com/adromir/scripts/raw/refs/heads/main/userscripts/gemini-snippets/google_gemini_mod.user.js
